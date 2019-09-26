8e296ae2da4ec9375c9afadf0e27cf8876ce79b6
xingnliu <xingnliu@dhcp-10-140-42-94.cisco.com>
2019-09-26 18:00:18 +0800

    move properties to code. It looks like I mess it up

--- SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
+++ SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
@@ -37,51 +37,62 @@ public class AuthServiceApplication extends WebMvcConfigurerAdapter {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
-	@Configuration
-	@Order(-20)
-	protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+}
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.formLogin().loginPage("/login").permitAll().and().requestMatchers()
-					.antMatchers("/login*", "/oauth/authorize", "/oauth/confirm_access").and().authorizeRequests()
-					.antMatchers("/login*").permitAll().anyRequest().authenticated().and().csrf().disable();
-		}
+@Configuration
+@Order(-20)
+class LoginConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
+				.antMatchers("/login*", "/oauth/authorize", "/oauth/confirm_access").permitAll().anyRequest()
+				.authenticated().and().csrf().disable();
 	}
 
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+}
 
-		@Autowired
-		private AuthenticationManager authenticationManager;
+@Configuration
+@EnableAuthorizationServer
+class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
-		@Bean
-		public JwtAccessTokenConverter jwtAccessTokenConverter() {
-			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-			KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray())
-					.getKeyPair("test");
-			converter.setKeyPair(keyPair);
-			return converter;
-		}
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory().withClient("m1").secret("s1")
-					.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid")
-					.autoApprove(true);
-		}
+//	@Bean
+//	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//		KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+//				.getKeyPair("test");
+//		converter.setKeyPair(keyPair);
+//		return converter;
+//	}
 
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
-		}
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("m1").secret("s1")
+				.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid")
+				.autoApprove(true);
+	}
 
-		@Override
-		public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
-		}
+//	@Override
+//	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//		endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+//	}
 
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+}
+
+@Configuration
+class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("m1").secret("s1").scopes("openid")
+				.authorizedGrantTypes("authorization_code,refresh_token,password")
+				.redirectUris("http://localhost:8080/login");
 	}
 }
