45a2081395ade2c236592a9a228365d882dc4d5e
xingnliu <xingnliu@dhcp-10-140-42-82.cisco.com>
2019-10-08 17:09:10 +0800

    Debugging...

--- SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
+++ SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
