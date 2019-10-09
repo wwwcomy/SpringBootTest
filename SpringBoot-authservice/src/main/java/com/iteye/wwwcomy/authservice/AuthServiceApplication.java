5e5b411bb87836307a87bd990d8d68112db17567
xingnliu <xingnliu@dhcp-10-140-42-115.cisco.com>
2019-10-09 16:29:28 +0800

    well, it is working now for client credentials flow. Although I don't know why...

--- SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
+++ SpringBoot-authservice/src/main/java/com/iteye/wwwcomy/authservice/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package com.iteye.wwwcomy.authservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,6 +21,9 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableAuthorizationServer
+@EnableWebSecurity
 public class AuthServiceApplication extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -35,23 +40,31 @@ public class AuthServiceApplication extends WebMvcConfigurerAdapter {
 
 @Configuration
 //@Order(-20)
-class LoginConfig extends WebSecurityConfigurerAdapter {
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) 
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.formLogin().loginPage("/login").permitAll().and().authorizeRequests().antMatchers("/login*").permitAll()
+//				.anyRequest().authenticated().and().csrf().disable();
+//	}
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().loginPage("/login").permitAll().and().authorizeRequests().antMatchers("/login*", "/oauth/*")
-				.permitAll().anyRequest().authenticated().and().csrf().disable();
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests().antMatchers("/**").authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+    }
 
 }
 
 @Configuration
-@EnableResourceServer
 class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) {
-		resources.resourceId("USERS").stateless(true);
+		resources.resourceId("USERS");
+		//.stateless(true);
 	}
 
 	@Override
@@ -60,15 +73,16 @@ class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 				// Since we want the protected resources to be accessible in the UI as well we
 				// need
 				// session creation to be allowed (it's disabled by default in 2.0.6)
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().requestMatchers()
-				.anyRequest().and().anonymous().and().authorizeRequests()
+//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+				.requestMatchers()
+                .antMatchers("/**")
+				.and().authorizeRequests()
 //                .antMatchers("/product/**").access("#oauth2.hasScope('select') and hasRole('ROLE_USER')")
-				.antMatchers("/user/**").authenticated();// 配置 order 访问控制，必须认证过后才可以访问
+				.antMatchers("/user").authenticated();// 配置 order 访问控制，必须认证过后才可以访问
 	}
 }
 
 @Configuration
-@EnableAuthorizationServer
 class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
@@ -87,7 +101,7 @@ class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("m1").secret("s1")
 				.authorizedGrantTypes("authorization_code", "refresh_token", "password", "client_credentials")
-				.scopes("openid").redirectUris("http://localhost:8080/login").autoApprove(true);
+				.scopes("openid").redirectUris("http://wwwcomy.com:8080/login").autoApprove(true);
 	}
 
 //	@Override
